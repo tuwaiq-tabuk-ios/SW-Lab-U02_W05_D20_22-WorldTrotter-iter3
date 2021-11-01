f7e1f5a53b9df4f92512ecc449d3bdd977f57785
عبدالعزيز البلوي <bdalzyzalblwy@MacBook-Air-bdalzyz.local>
2021-11-01 11:03:46 +0300

    finish home work

---
+++ chapter.5/WorldTrotter-iter3-ConversionViewController-silver 2/WorldTrotter-iter1/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  WorldTrotter-iter1
+//
+//  Created by عبدالعزيز البلوي on 19/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+ 
+    //let gradient = CAGradientLayer()
+
+     //   gradient.frame = view.bounds
+  //  gradient.colors = [UIColor.blue.cgColor, UIColor.red.cgColor,UIColor.yellow.cgColor,UIColor.green.cgColor, UIColor.systemPink.cgColor,UIColor.systemIndigo.cgColor,UIColor.gray.cgColor]
+
+       // view.layer.insertSublayer(gradient, at: 0)
+}
+  override func viewWillAppear(_ animated: Bool) {
+      var randomFloat: CGFloat {
+          return CGFloat(Float.random(in: 0.0...1.0))
+      }
+      
+        let randomColor = UIColor(red: randomFloat, green: randomFloat, blue: randomFloat, alpha: 1.0)
+        view.backgroundColor = randomColor
+    }
+
+  
+}
