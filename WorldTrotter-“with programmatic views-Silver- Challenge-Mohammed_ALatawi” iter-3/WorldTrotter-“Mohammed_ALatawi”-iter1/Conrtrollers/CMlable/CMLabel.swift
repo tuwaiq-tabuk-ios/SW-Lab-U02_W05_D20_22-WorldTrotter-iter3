ddb2e8ba0b3bf8dbcaba7d4b9b99e6a928b10c20
mohammed <mhmdalsbwty@gmail.com>
2021-11-01 11:10:24 +0300

    WorldTrotterIterFinished

---
+++ WorldTrotter-“with programmatic views-Silver- Challenge-Mohammed_ALatawi” iter-3/WorldTrotter-“Mohammed_ALatawi”-iter1/Conrtrollers/CMlable/CMLabel.swift
@@ -0,0 +1,36 @@
+//
+//  CMLabel.swift
+//  WorldTrotter-“Mohammed_ALatawi”-iter1
+//
+//  Created by محمد العطوي on 26/03/1443 AH.
+//
+
+import UIKit
+
+class CmLabel: UILabel {
+  
+  override init(frame: CGRect) {
+    super .init(frame: frame)
+  }
+  required init?(coder:NSCoder){
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  init( text: String ,
+        textSize: CGFloat,
+        textColor: UIColor = UIColor.black){
+    
+    super.init(frame: .zero)
+    self.text = text
+    self.font = UIFont.systemFont(ofSize: textSize)
+    self.textColor = textColor
+    
+    customize()
+    
+  }
+  
+  private func customize() {
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+}
---
+++ WorldTrotter-“with programmatic views-Silver- Challenge-Mohammed_ALatawi” iter-3/WorldTrotter-“Mohammed_ALatawi”-iter1/Conrtrollers/ConversionViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter-“Mohammed_ALatawi”-iter1
+//
+//  Created by محمد العطوي on 22/03/1443 AH.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+  override func loadView() {
+    view = UIView()
+    
+    view.backgroundColor = UIColor.cmWhiteBackground
+    
+    
+    let valueFarenheitLabel = CmLabel(text: "212",
+                                      textSize: 70,
+                                      textColor:UIColor.cmOrange)
+    
+    view.addSubview(valueFarenheitLabel)
+    
+    let titleFarenheitLabel = CmLabel(text: "degrees Fahrenheit",
+                                      textSize: 36,
+                                      textColor: UIColor.cmOrange)
+    
+    view.addSubview(titleFarenheitLabel)
+    
+    let isReallyLabel = CmLabel(text: "is really",
+                                textSize: 36,
+                                textColor: .black)
+    
+    view.addSubview(isReallyLabel)
+    
+    let valueCelsiusLabel = CmLabel(text: "100",
+                                    textSize: 70,
+                                    textColor: UIColor.cmOrange)
+    
+    view.addSubview(valueCelsiusLabel)
+    
+    let titleCelsiusLabel = CmLabel(text: "degrees Celsius",
+                                    textSize: 36,
+                                    textColor: UIColor.cmOrange)
+    
+    view.addSubview(titleCelsiusLabel)
+    
+    
+    NSLayoutConstraint.activate([
+                                  valueFarenheitLabel.topAnchor .constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 8),
+                                  
+                                  
+                                  valueFarenheitLabel.centerXAnchor .constraint(equalTo: view.centerXAnchor),
+                                  
+                                  
+                                  titleFarenheitLabel.topAnchor .constraint(equalTo: valueFarenheitLabel.bottomAnchor,constant:10),
+                                  
+                                  titleFarenheitLabel.centerXAnchor
+                                    .constraint(equalTo: view.centerXAnchor),
+                                  
+                                  
+                                  isReallyLabel.topAnchor .constraint(equalTo: titleFarenheitLabel.bottomAnchor,constant: 40),
+                                  
+                                  isReallyLabel.centerXAnchor .constraint(equalTo: view.centerXAnchor),
+                                  
+                                  
+                                  valueCelsiusLabel.topAnchor.constraint(equalTo: isReallyLabel.bottomAnchor,constant: 40),
+                                  
+                                  valueCelsiusLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                                  
+                                  
+                                  titleCelsiusLabel.topAnchor .constraint(equalTo: valueCelsiusLabel.bottomAnchor,constant: 10),
+                                  
+                                  
+                                  titleCelsiusLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)])
+    
+    
+  }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print(" ConversionViewController loaded VIew ")
+    
+  }
+  
+  
+}
